02c4885ca450e5dfd5012addcd891382560ca51a
Raniel Mendes <raniel.mendes@hotmail.com>
2022-02-10 01:27:40 -0300

    [Feature Rani - Task: Desafio #5.1]

---
+++ Desafio5.1/desafio-cinco-um/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'desafio-cinco-um'
+  nome = "Raniel Mendes Generoso, pode me chamar de Rani!"
+  tutor = "Elivel Santos Nascimento"
+  data = "Hoje é quarta-feira dia 09 de Fevereiro às 19:19hs GMT-0300 (Horário de Brasília)"
+  turma= "Faço parte da melhor equipe da NTT Data, Equipe Só Vamu!";
+}
