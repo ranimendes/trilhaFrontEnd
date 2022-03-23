dc9173b21b5090d5f4dac3224dfd3eaa17954a9c
Raniel Mendes <raniel.mendes@hotmail.com>
2022-03-23 18:34:05 -0300

    [Feature Rani - Task: Update Regras de negócio]

--- Simulador Financeiro/simulador-financeiro/src/app/pages/pages.service.ts
+++ Simulador Financeiro/simulador-financeiro/src/app/pages/pages.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Cliente } from './form-client/shared/cliente.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PagesService {
+  showMessage(arg0: string) {
+    throw new Error('Method not implemented.');
+  }
 
-  constructor() { }
+  baseUrl = 'http://localhost:3000/posts';
+
+  constructor(private http: HttpClient) {}
+
+  enviar(client: any): Observable<Cliente> {
+    console.log('Serviço respondendo!', client);
+    return this.http.post<Cliente>(this.baseUrl, client);
+  }
 }
