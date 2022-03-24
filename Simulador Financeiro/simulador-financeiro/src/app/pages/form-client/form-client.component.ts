77f36545f2e56e7e632c50202f763458346e1bd5
Raniel Mendes <raniel.mendes@hotmail.com>
2022-03-24 17:34:08 -0300

    [Feature Rani - Task: Update component #2]

--- Simulador Financeiro/simulador-financeiro/src/app/pages/form-client/form-client.component.ts
+++ Simulador Financeiro/simulador-financeiro/src/app/pages/form-client/form-client.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
 import {
   FormBuilder,
   FormControl,
@@ -10,45 +10,57 @@ import { Cliente } from './shared/cliente.model';
 import { ValidaClient } from './valida-client.module';
 import { ClientStorageService } from './shared/client-storage.service';
 import { Router } from '@angular/router';
+import { BasicInfoService } from 'src/app/shared/Service/basic-info.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-form-client',
   templateUrl: './form-client.component.html',
   styleUrls: ['./form-client.component.css'],
 })
-export class FormClientComponent implements OnInit {
+export class FormClientComponent extends BasicInfoService implements OnInit {
+
   private onlyNumber = '[0-9]*';
   private caracteres = '[A-zÀ-ú ]+';
+  enviaForm = this.botaoSalvar();
 
   formClient!: FormGroup;
 
   constructor(
-    private fb: FormBuilder,
-    private clientStorage: ClientStorageService,
-    private router: Router
-  ) {}
+    injector: Injector,
+    location: Location,
+    router: Router,
+    protected fb: FormBuilder,
+    protected clientStorage: ClientStorageService,
+  ) {
+    super(injector, location, router)
+  }
 
   ngOnInit(): void {
+    this.geraTitulo(this.imprimeTitulo());
     this.criarFormulario();
   }
 
   client() {}
 
-  navigateImovel() {
-    const client: Cliente = new Cliente(
-      this.formClient.get('id')?.value,
-      this.formClient.get('nome')?.value,
-      this.formClient.get('trabalho')?.value,
-      this.formClient.get('cpf')?.value,
-      this.formClient.get('email')?.value,
-      this.formClient.get('dataDeNascimento')?.value,
-      this.formClient.get('cep')?.value,
-      this.formClient.get('celular')?.value
-    );
-    this.clientStorage.setClient(client);
-      
-    this.router.navigate(['form-imovel']);
+  imprimeTitulo():string {
+    return 'Formulário do Cliente'
   }
+
+  // navigateImovel() {
+  //   const client: Cliente = new Cliente(
+  //     this.formClient.get('id')?.value,
+  //     this.formClient.get('nome')?.value,
+  //     this.formClient.get('trabalho')?.value,
+  //     this.formClient.get('cpf')?.value,
+  //     this.formClient.get('email')?.value,
+  //     this.formClient.get('nascimento')?.value,
+  //     this.formClient.get('cep')?.value,
+  //     this.formClient.get('celular')?.value
+  //   );
+  //   this.clientStorage.setClient(client);
+  //   this.router.navigate(['form-imovel']);
+  // }
 
   private criarFormulario() {
     this.formClient = this.fb.group({
@@ -57,7 +69,7 @@ export class FormClientComponent implements OnInit {
         Validators.pattern(this.caracteres),
         Validators.minLength(3),
       ]),
-      trabalho: new FormControl(null, [Validators.required]),
+      trabalho: new FormControl(null, [Validators.required], ),
       cpf: new FormControl(null, [
         Validators.required,
         Validators.pattern(this.onlyNumber),
@@ -81,4 +93,19 @@ export class FormClientComponent implements OnInit {
       ]),
     });
   }
+
+  protected botaoSalvar(){
+    const url = this.location.path();
+    this.enviarForm = true;
+
+    if (url == "/form-client")
+      this.rota = this.criaRotaImovel();
+      return this.rota;
+  }
+
+  protected criaRotaImovel(){
+    return "/form-imovel"
+  }
+
+
 }
