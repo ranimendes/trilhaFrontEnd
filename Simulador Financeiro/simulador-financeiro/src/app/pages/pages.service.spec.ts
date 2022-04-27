c65844c18963b687ea39ad75afba4698bdd4e8c3
Raniel Mendes <raniel.mendes@hotmail.com>
2022-04-27 01:59:16 -0300

    [Feature Rani - Task: update full ]

--- Simulador Financeiro/simulador-financeiro/src/app/pages/pages.service.spec.ts
+++ Simulador Financeiro/simulador-financeiro/src/app/pages/pages.service.spec.ts
@@ -1,11 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { PagesService } from './pages.service';
 
-describe('PagesService', () => {
+fdescribe('PagesService', () => {
   let service: PagesService;
 
-  beforeEach(() => {
+   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(PagesService);
   });
