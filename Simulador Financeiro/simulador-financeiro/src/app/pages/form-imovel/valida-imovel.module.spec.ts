0c20b171b0d47be6f40ccb3e01494f310cf5e7ef
Raniel Mendes <raniel.mendes@hotmail.com>
2022-04-29 03:52:23 -0300

    [Feature Rani - Task: Valida Imovel Module Spec Ts ]

--- Simulador Financeiro/simulador-financeiro/src/app/pages/form-imovel/valida-imovel.module.spec.ts
+++ Simulador Financeiro/simulador-financeiro/src/app/pages/form-imovel/valida-imovel.module.spec.ts
@@ -2,16 +2,11 @@ import { AbstractControl, FormControl } from '@angular/forms'
 import { ValidaImovel } from './valida-imovel.module'
 
 describe('ValidaImovel Module ', () => {
-  it('Valida Valor Entrada', () => {
-    const mockCampo: AbstractControl = new FormControl(250)
-    let result = ValidaImovel.valorDaEntrada(mockCampo);
-    expect(result).toEqual(null)
-  })
-
-  it('Valida Valor Entrada Null', () => {
+  it('Valida Entrada', () => {
     const mockCampo: AbstractControl = new FormControl(0)
     let result = ValidaImovel.valorDaEntrada(mockCampo);
     expect(result).toEqual(null)
   })
+
 
 })
